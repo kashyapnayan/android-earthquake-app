afecfa1b622991ce52af94e6d9ab66792a0bbf92
Lavi Kashyap <lavi.bhumca2015@gmail.com>
2021-09-02 19:31:06 +0530

    Date Formatting

--- app/src/main/java/com/example/earthquakeapp/EarthquakeAdapter.java
+++ app/src/main/java/com/example/earthquakeapp/EarthquakeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.earthquakeapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
@@ -30,22 +33,30 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         Earthquake earthquake = getItem(position);
 
         // Find the TextView in the earthquake_list_item.xml layout with the ID magnitude.
-        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
+        TextView magnitudeTextView =  listItemView.findViewById(R.id.magnitude);
         // Get the Magnitude translation from the currentWord object and set this text on
         // the Magnitude TextView.
         magnitudeTextView.setText(String.valueOf(earthquake.getMagnitude()));
 
         // Find the TextView in the earthquake_list_item.xml layout with the ID location.
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+        TextView locationTextView =  listItemView.findViewById(R.id.location);
         // Get the Location translation from the currentWord object and set this text on
         // the Location TextView.
         locationTextView.setText(earthquake.getLocation());
 
         // Find the TextView in the earthquake_list_item.xml layout with the ID date.
-        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        TextView dateTextView =  listItemView.findViewById(R.id.date);
         // Get the Date translation from the currentWord object and set this text on
         // the Date TextView.
-        dateTextView.setText(String.valueOf(earthquake.getDate()));
+
+        long timeInMilliseconds = earthquake.getDate();
+        Date dateObject = new Date(timeInMilliseconds);
+
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MM dd, yyyy");
+        String actualDate = dateFormatter.format(dateObject);
+
+        dateTextView.setText(actualDate);
 
         return  listItemView;
     }
--- app/src/main/java/com/example/earthquakeapp/QueryUtils.java
+++ app/src/main/java/com/example/earthquakeapp/QueryUtils.java
@@ -39,17 +39,18 @@ public class QueryUtils {
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
 
-            // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
             // build up a list of Earthquake objects with the corresponding data.
             JSONObject jsonObject = new JSONObject(SAMPLE_JSON_RESPONSE);
             JSONArray jsonArray = jsonObject.optJSONArray("features");
-            for(int i = 0; i < jsonArray.length(); i++){
+            for(int i = 0; i < (jsonArray != null ? jsonArray.length() : 0); i++){
                 JSONObject earthquakeJsonObject = jsonArray.getJSONObject(i);
                 JSONObject propertiesJsonObject = earthquakeJsonObject.optJSONObject("properties");
-                earthquakes.add(new Earthquake(
-                        propertiesJsonObject.getDouble("mag"),
-                        propertiesJsonObject.getString("place"),
-                        propertiesJsonObject.getLong("time")));
+                if(propertiesJsonObject!=null){
+                    earthquakes.add(new Earthquake(
+                            propertiesJsonObject.getDouble("mag"),
+                            propertiesJsonObject.getString("place"),
+                            propertiesJsonObject.getLong("time")));
+                }
             }
 
         } catch (JSONException e) {
